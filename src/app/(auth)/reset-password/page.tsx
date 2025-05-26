561c675c42d4c30d2df79cfe8a196ce7e2ff01f8
spadaritucks <thiago.spadari02@gmail.com>
2025-05-26 15:23:59 -0300

    renomeado o componente de Login para ResetPassword; atualizado o layout para melhorar a responsividade e a usabilidade; integrado o componente FormInput para os campos de entrada, mantendo a consistência com as alterações anteriores.

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,55 +1,70 @@
+import FormInput from '@/components/formInput/FormInput'
 import { Button } from '@/components/ui/button'
 import { Mail } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-export default function Login() {
+export default function ResetPassword() {
+
   return (
-    <div className='h-screen bg-zinc-100 grid place-items-center'>
-      <div className='grid grid-cols-2 max-w-5xl w-full bg-white rounded-2xl h-2/3 shadow-md gap-10'>
-        <div className='bg-gradient-to-br text-white from-blue-600 via-sky-500 to-lime-100 p-5 rounded-tl-2xl rounded-bl-2xl'>
-          <div className='flex flex-col justify-evenly items-center h-3/4 space-y-6'>
-            <div className='inline-flex items-center justify-center space-x-4'>
-              <Image src={"/logo.png"} alt='droplet logo' width={75} height={75} />
-              <p className='font-black text-5xl'>DROPLET</p>
+
+    <section className="flex flex-col lg:h-screen lg:grid place-items-center bg-zinc-100">
+      <div className="flex flex-col lg:grid grid-cols-2 gap-10 rounded-2xl bg-white shadow-md mt-20 lg:mt-0">
+        <div className="h-full rounded-tl-2xl rounded-bl-2xl bg-gradient p-5 text-white">
+          <div className="flex h-3/4 flex-col items-center justify-evenly space-y-6">
+            <div className="inline-flex items-center justify-center space-x-4">
+              <Image src="/logo.png" alt="droplet logo" width={50} height={50} />
+              <p className="text-4xl lg:text-5xl font-black">DROPLET</p>
             </div>
-            <p className='text-center font-bold text-xl px-8'>
+            <p className="px-8 text-center text-xl font-bold">
               Liberdade para criar
             </p>
           </div>
         </div>
-        <div className='py-5 flex flex-col gap-5 justify-evenly'>
-          <p className='text-2xl font-semibold'>Recuperação de senha</p>
-          <form action="" className='flex flex-col gap-5'>
-            <div className='bg-zinc-100 rounded-full px-4 py-5 w-11/12 flex gap-4 text-zinc-600'>
-              <Mail />
-              <input placeholder='E-mail' type="text" className='w-full bg-zinc-100 focus:outline-none font-medium text-zinc-600' />
+
+        <div className="flex flex-col gap-5 p-5">
+          <p className="text-2xl font-semibold">Recuperação de senha</p>
+          
+          <form action="" className="flex flex-col gap-5">
+            <div className="flex flex-col gap-2">
+              <div className="flex w-11/12 items-center gap-2">
+                <Mail />
+                <FormInput placeholder="E-mail" type="email"/>
+              </div>
             </div>
-            <div className='rounded-md px-2 py-4 w-11/12 flex flex-col gap-2 text-zinc-600'>
-              <Button  className='w-full rounded-full uppercase font-bold'>Enviar por e-mail</Button>
-              <div className='flex justify-between p-2'>
-                <Link className='hover:text-zinc-800' href={"/login"}>Entrar</Link>
-                <Link className='hover:text-zinc-800' href={"/register"}>Inscreva-se</Link>
+
+            <div className="flex w-11/12 flex-col gap-2 rounded-md px-2 py-4 text-zinc-600">
+              <Button className="rounded-full font-bold uppercase">Enviar por e-mail</Button>
+              <div className="flex justify-between p-2">
+                <Link className="hover:text-zinc-800" href="/login">
+                  Entrar
+                </Link>
+                <Link className="hover:text-zinc-800" href="/register">
+                  Inscreva-se
+                </Link>
               </div>
             </div>
           </form>
-            <div className='flex flex-col gap-5'>
-              <div className='rounded-md px-2 w-11/12 flex gap-2 text-white'>
-                <Button className='w-full bg-sky-white rounded-full text-black hover:bg-zinc-200 border items-center flex gap-10'> 
-                <Image src={"/google-logo.png"} alt='google logo' width={30} height={30} />
-                <span className='uppercase font-bold'>Iniciar sessão com o google</span>
-                </Button>
-              </div>
-              <div className='rounded-md px-2 w-11/12 flex gap-2'>
-              <Button className='w-full bg-sky-white rounded-full text-black hover:bg-zinc-200 border items-center flex gap-10'>
-              <Image src={"/twitter-x-logo.png"} alt='twitter logo' width={30} height={30} />
-              <span className='uppercase font-bold'>Iniciar sessão com o twitter</span>
+
+          <div className="flex flex-col gap-5">
+            <div className="flex gap-2 rounded-md text-white">
+              <Button className="flex w-full items-center gap-4 px-8 rounded-full border bg-sky-white text-black hover:bg-zinc-200">
+                <Image src="/google-logo.png" alt="google logo" width={25} height={25} />
+                <span className="font-bold uppercase text-sm">Iniciar sessão com o google</span>
               </Button>
-              </div>
             </div>
+
+            <div className="flex gap-2 rounded-md">
+              <Button className="flex w-full items-center gap-4 px-8 rounded-full border bg-sky-white text-black hover:bg-zinc-200">
+                <Image src="/twitter-x-logo.png" alt="twitter logo" width={25} height={25} />
+                <span className="font-bold uppercase text-sm">Iniciar sessão com o twitter</span>
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
+    
   )
 }
